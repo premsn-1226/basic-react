a3186af26b4a9b61d28db45eedc637f737921166
prem S N <prem.sn.eee@gmail.com>
2020-07-02 13:45:00 +0530

    component part1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className = "App-header">
-        <img src={logo}  className="App-logo" alt="logo" />
-        <p>this is a react project</p>
-        <h1>this is a react paragraph</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar dark color ="primary">
+        <div className="container">
+        <NavbarBrand href="#"><img src={logo}  className="App-logo" alt="logo" />sri saravana hotel</NavbarBrand>
+        </div>
+    </Navbar>
+    <Menu />
     </div>
   );
 }
